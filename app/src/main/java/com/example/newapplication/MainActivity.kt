fe5ee5d2aeb299d81132b972641cb5f3164e364d
Jason Rahanetra <jrahanetra@gmail.com>
2024-07-05 08:42:34 +0300

    First commit

--- app/src/main/java/com/example/newapplication/MainActivity.kt
+++ app/src/main/java/com/example/newapplication/MainActivity.kt
@@ -45,7 +45,7 @@ import com.example.newapplication.ui.theme.NewApplicationTheme
 
 @Suppress("DEPRECATION")
 class MainActivity : ComponentActivity() {
-    override fun onCreate(saDSAvedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //ICI NOUS ASSIGNONS L'ATTRIBUT DE L'OBJET LISTPRODUIT DANS LE VAL PRODUITS
 
