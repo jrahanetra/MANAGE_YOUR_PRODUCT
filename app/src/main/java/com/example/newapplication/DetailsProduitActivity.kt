705bf930a573ec72ba19b5048d0dfc0f3a367999
Jason Rahanetra <jrahanetra@gmail.com>
2024-07-06 12:17:29 +0300

    CHANGE AND ADD SOME FILES

--- app/src/main/java/com/example/newapplication/DetailsProduitActivity.kt
+++ app/src/main/java/com/example/newapplication/DetailsProduitActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -26,7 +25,6 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
